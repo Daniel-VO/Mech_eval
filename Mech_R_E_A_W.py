1405c22a467e16f42aecd24f38aafd2ae66d25ca
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2022-12-05 08:12:19 +0100

    Add files via upload

--- Mech_R_E_A_W.py
+++ Mech_R_E_A_W.py
@@ -1,5 +1,5 @@
 """
-Created 24. October 2022 by Daniel Van Opdenbosch, Technical University of Munich
+Created 05. December 2022 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -14,6 +14,14 @@ import matplotlib.pyplot as plt
 import matplotlib.patheffects as pe
 from scipy import signal
 from scipy import stats
+
+def Bahadur(f,wDehnung,wSpannung):
+	slope,intercept,low_slope,high_slope=stats.theilslopes(numpy.log(wSpannung),x=wDehnung)
+	m,sigma0=slope,numpy.exp(intercept)
+	plt.plot(wDehnung,sigma0*numpy.exp(m*wDehnung))
+	plt.plot(wDehnung,wSpannung)
+	plt.savefig(str(os.path.splitext(f)[0])+'_Bahadur.png')
+	return m,sigma0
 
 Dehngrenze=2e-4		#
 
@@ -33,7 +41,7 @@ sys.stdout=open('Results.log','a')
 files=glob.glob('*[!_corr].txt',recursive=True)
 
 @ray.remote
-def mech(f,Dehngrenze,L,alpha):
+def mech(f,Dehngrenze,L,alpha,*args):
 	filename=os.path.splitext(f)[0].split('/')[-1]
 	print(filename)
 	if 'Weg_G_mm' in str(open(f,'r').readlines()):
@@ -54,7 +62,7 @@ def mech(f,Dehngrenze,L,alpha):
 	theils=numpy.array([])
 	for i in numpy.arange(steps):
 		ind=numpy.where((Dehnung>=i*Agt0/steps)&(Dehnung<(1+i)*Agt0/steps))
-		theils=numpy.append(theils,stats.theilslopes(Spannung[ind],Dehnung[ind]))
+		theils=numpy.append(theils,stats.theilslopes(Spannung[ind],x=Dehnung[ind]))
 	theils=theils.reshape(steps,4)
 	theil=theils[numpy.where(theils[:,0]==max(theils[:,0]))][0]
 	E=theil[0]
@@ -80,6 +88,11 @@ def mech(f,Dehngrenze,L,alpha):
 
 	Wt=numpy.trapz(Spannung[:indBruch],x=Dehnung[:indBruch])
 	W=Wt-R**2/E/2
+
+	if 'Bahadur' in [*args]:
+		Bereich=numpy.where(Dehnung-Spannung/E<Dehngrenze)
+		m,sigma0=Bahadur(f,numpy.log(Dehnung[Bereich]+1),Spannung[Bereich]*(Dehnung[Bereich]+1))
+		print(filename,'m',m,'sigma0',sigma0)
 
 	print(filename,'R',R,'E',E,'A',A,'W',W,'Re',R,'Ag',Ag,'At',At,'Wt',Wt)
 	numpy.savetxt(filename+'_corr.txt',numpy.transpose([Dehnung,Spannung]))
@@ -125,7 +138,7 @@ def mech(f,Dehngrenze,L,alpha):
 
 	return filename,R,E,A,W,Re,Ag,At,Wt
 
-data=ray.get([mech.remote(f,Dehngrenze,L,alpha) for f in files])
+data=ray.get([mech.remote(f,Dehngrenze,L,alpha,sys.argv) for f in files])
 
 numpy.save('data.npy',data)
 os.system('python3 read_Mech_R_E_A_W.py')
